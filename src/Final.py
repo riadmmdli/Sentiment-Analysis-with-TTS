4dc7082512bceb45c5599244fa96a150152b0456
Riad Mammadli <riadmammadli2003@gmail.com>
2025-02-23 19:24:06 +0300

    Update Final.py
    silindi

--- src/Final.py
+++ src/Final.py
@@ -12,7 +12,7 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from tkinter import messagebox, simpledialog, Listbox
 
-#Merhaba
+
 
 # Load the fine-tuned Turkish BERT model, tokenizer, and label encoder
 model_path = "C:/Users/riadm/Desktop/BertTurkModelFinal/fine_tuned_results_turkish"
